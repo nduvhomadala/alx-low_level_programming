0e134d64ad5f5afcf301d4ec95c3e5e9f3058eab
nduvhomadala <nduvhomadala@gmail.com>
2023-06-20 08:04:52 -0700

    commit2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 
  /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * Return: void
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
@@ -11,13 +11,13 @@ int i;
 i = 0;
 while (i < 10)
 {
-for (ch = 'a'; ch <= 'z'; ch++)
+ch = 'a';
+while (ch <= 'z')
 {
 _putchar(ch);
+ch++;
 }
 _putchar('\n');
 i++;
 }
-
-
 }
